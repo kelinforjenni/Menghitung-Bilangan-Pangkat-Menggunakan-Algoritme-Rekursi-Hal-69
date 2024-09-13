3130ebb2fa670b0231851e33bf9ce8e6ad225f74
kelinforjenni <kelinforjenni@gmail.com>
2024-09-13 13:14:16 +0700

    Add files via upload

---
+++ Menghitung Bilangan Pangkat Menggunakan Algoritme Rekursi.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int Pangkat (int x, int y)
+{
+    if (y != 0)
+    return (x*Pangkat(x, y-1));
+    else
+    return 1;
+}
+int main()
+{
+    int a, n, hasil;
+    cout << "Masukkan bilangan dasar: ";
+    cin >> a;
+    cout << "Masukkan bilangan pangkat(bulat positif): ";
+    cin >> n;
+    hasil = Pangkat(a, n);
+cout << "Hasil " << a << "^" << n << " = " << hasil <<endl;
+    system ("PAUSE");
+    return 0;
+}
+    
